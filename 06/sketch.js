c1cf0e8c030bc2fc4c024ccbcf02d1c634552241
AmandaPachecoB <31419458+AmandaPachecoB@users.noreply.github.com>
2017-10-22 21:55:24 -0500

    Add files via upload

---
+++ 06/sketch.js
@@ -0,0 +1,31 @@
+
+var peacock;
+var x = 0;
+var y = 0;
+
+
+function preload() {
+  peacock = loadImage('Assets/Peacock2.jpg');
+}
+
+function setup() {
+  createCanvas(800, 450);
+}
+
+function draw() {
+  peacock.loadPixels();
+  var pix = peacock.get(x, y);
+
+  fill(pix[0], pix[1], pix[2], pix[3]);
+  strokeWeight(0);
+  ellipse(x, y, 10, 10);
+  
+
+  x += 10
+  if (x > 800) {
+    x = 0
+    y += 10
+  }
+
+
+}
